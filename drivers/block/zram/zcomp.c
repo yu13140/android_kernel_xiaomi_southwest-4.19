625aea8c257c5b7f0cea5dabc4a00f920a032596
yu13140 <whmyc801@gmail.com>
2025-11-03 00:05:47 +0800

    compression: add lz4k_oplus algorithms Introduce lz4kd_oplus compression algorithms to enhance kernel compression capabilities. Reference from - https://github.com/SukiSU-Ultra/SukiSU_patch/blob/main/other/zram/zram_patch/5.10/lz4k_oplus.patch - https://github.com/TheGeniusClub/android_kernel_common_oneplus_sm8750/commit/2e1507f84cb2162b4bc8fbad814df8672d58aa73
    Co-authored-by: ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
    Co-authored-by: brokestar233 <3765589194@qq.com>
    Change-Id: I2fd5bc0dea7eadb3a505c1ceb3e53a1e661f4036
    Signed-off-by: yu13140 <whmyc801@gmail.com>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -35,6 +35,9 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 	"zstd",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4K_OPLUS)
+        "lz4k_oplus",
+#endif
 };
 
 static void zcomp_strm_free(struct zcomp_strm *zstrm)
